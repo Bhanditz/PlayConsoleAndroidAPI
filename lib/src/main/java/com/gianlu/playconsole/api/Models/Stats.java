b9955f0d41e172899a22e0838f72cd6e27475799
Gianlu <altomanigianluca@gmail.com>
2017-07-06 12:40:20 +0200

    Entries sorting and some more helper methods

--- lib/src/main/java/com/gianlu/playconsole/api/Models/Stats.java
+++ lib/src/main/java/com/gianlu/playconsole/api/Models/Stats.java
@@ -1,11 +1,15 @@
 package com.gianlu.playconsole.api.Models;
 
+import android.support.annotation.Nullable;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
 import java.util.TimeZone;
@@ -24,6 +28,14 @@ public class Stats {
             entries.add(new Entry(entriesArray.getJSONObject(i)));
     }
 
+    /**
+     * Sorts entries evaluating their {@link Entry#value}.
+     * It is not recommended to run this on date based entries.
+     */
+    public void sortEntries() {
+        Collections.sort(entries, new EntryValueComparator());
+    }
+
     public SimpleDateFormat getDateParser() {
         SimpleDateFormat parser = new SimpleDateFormat("yyyyMMdd", Locale.getDefault());
         parser.setTimeZone(timezone);
@@ -34,15 +46,29 @@ public class Stats {
         return entries.size() > 1;
     }
 
-    public Entry firstEntry() {
-        return entries.get(0);
+    public static class EntryValueComparator implements Comparator<Entry> {
+        @Override
+        public int compare(Entry o1, Entry o2) {
+            if (o1.value == null && o2.value != null) return 1;
+            else if (o1.value != null && o2.value == null) return -1;
+            else if (o1.value == null /* && o2.value == null */) return 0;
+
+            float diff = o1.value - o2.value;
+            return diff == 0.0f ? 0 : (diff > 0.0f ? -1 : 1);
+        }
     }
 
     public class Entry {
         public final JSONObject obj;
+        public final String key;
+        public final String keyDescription;
+        public final Float value;
 
         public Entry(JSONObject obj) {
             this.obj = obj;
+            this.key = optKey();
+            this.keyDescription = optKeyDescription();
+            this.value = optValue();
         }
 
         /**
@@ -55,6 +81,15 @@ public class Stats {
             return obj.getJSONArray("1").getString(0);
         }
 
+        @Nullable
+        public String optKey() {
+            try {
+                return getKey();
+            } catch (JSONException ex) {
+                return null;
+            }
+        }
+
         /**
          * Return the formal representation of key of the entry (as it's usually)
          *
@@ -63,6 +98,15 @@ public class Stats {
          */
         public String getKeyDescription() throws JSONException {
             return obj.getJSONArray("4").getString(0);
+        }
+
+        @Nullable
+        public String optKeyDescription() {
+            try {
+                return getKeyDescription();
+            } catch (JSONException ex) {
+                return null;
+            }
         }
 
         /**
@@ -77,6 +121,15 @@ public class Stats {
             if (data.has("1")) return data.getLong("1");
             else if (data.has("2")) return (float) data.getDouble("2");
             else throw new RuntimeException("Cannot find value!");
+        }
+
+        @Nullable
+        public Float optValue() {
+            try {
+                return getValue();
+            } catch (JSONException | RuntimeException ex) {
+                return null;
+            }
         }
     }
 }
